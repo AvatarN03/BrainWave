b81b20bfa29b3bbb1916934b9bf39d8627c1e237
AvatarN03 <jsprashanth003@gmail.com>
2025-04-26 12:59:50 +0530

    fix error-1"

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -5,7 +5,7 @@ import Section from "./Section";
 import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
-import TagLine from "./Tagline";
+import TagLine from "./TagLine";
 
 const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
